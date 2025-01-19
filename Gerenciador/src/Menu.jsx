df1c04cc2ede693162f46f1feeeee9cda83120e4
Emanuelle <emanuellegmnsa@gmail.com>
2025-01-19 00:58:13 -0300

    gerenciador menu

---
+++ Gerenciador/src/Menu.jsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import SwapHorizOutlinedIcon from "@mui/icons-material/SwapHorizOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import DehazeOutlinedIcon from "@mui/icons-material/DehazeOutlined";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+
+export default function TemporaryDrawer() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState(null);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const handleMenuItemClick = (index) => {
+    setSelectedItem(index);
+    setOpen(false); // close the drawer after selecting an item
+  };
+
+  const menuItems = [
+    { text: "Dashboard", icon: <TimelineOutlinedIcon /> },
+    { text: "Fornecedores", icon: <StorefrontOutlinedIcon /> },
+    { text: "Produtos", icon: <ShoppingCartOutlinedIcon /> },
+    { text: "Compra", icon: <LocalMallOutlinedIcon /> },
+    { text: "Venda", icon: <MonetizationOnOutlinedIcon /> },
+    { text: "Transferencia", icon: <SwapHorizOutlinedIcon /> },
+  ];
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem key={"Fechar"} button onClick={() => toggleDrawer(false)}>
+          <ListItemButton>
+            <ListItemIcon>
+              {<ArrowBackIosNewOutlinedIcon></ArrowBackIosNewOutlinedIcon>}
+            </ListItemIcon>
+            <ListItemText primary={"Fechar"} />
+          </ListItemButton>
+        </ListItem>
+        {menuItems.map((item, index) => (
+          <ListItem
+            key={item.text}
+            button
+            onClick={() => handleMenuItemClick(index)}
+          >
+            <ListItemButton>
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  const renderMainContent = () => {
+    switch (selectedItem) {
+      case 1:
+        return <div></div>;
+      case 2:
+        return <div></div>;
+      case 3:
+        return <div></div>;
+      case 4:
+        return <div></div>;
+      case 5:
+        return <div></div>;
+      default:
+        return <div></div>;
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={toggleDrawer(true)}>
+        <DehazeOutlinedIcon></DehazeOutlinedIcon>
+      </Button>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      <div style={{ marginLeft: 100, marginTop: 10 }}>
+        {renderMainContent()}
+      </div>
+    </div>
+  );
+}
