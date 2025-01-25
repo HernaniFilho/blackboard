73713f4106936e7296d4a4cb0e5f8079e3322823
Emanuelle <emanuellegmnsa@gmail.com>
2025-01-25 13:33:39 -0300

    Create produto

---
+++ Gerenciador/src/assets/palette.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    custom: {
+      white: "#FFFFFF",
+      beige: "#F5EFEB",
+      skyBlue: "#C8D9E6",
+      teal: "#567C8D",
+      navy: "#2F4156",
+      green: "#8bc34a",
+      red: "#ff1744",
+    },
+  },
+});
+
+export default theme;
