cbf9f6178188db642766658dce8397493d43d42e
Leonardo Yocogawa <leonardoyocogawa@gmail.com>
2025-01-27 21:53:41 -0300

    Ajustes useEffect de tabela.jsx

--- Loja_Varejo_1/src/Components/Tabela/tabela.jsx
+++ Loja_Varejo_1/src/Components/Tabela/tabela.jsx
@@ -262,6 +262,11 @@ export default function TableProducts() {
   fetchProdutos();
   }, []);*/
 
+
+
+
+  //GET
+  React.useEffect(() => {
   async function fetchProdutos() {
     try {
       console.log("To em fetchProdutos 1");
@@ -271,10 +276,10 @@ export default function TableProducts() {
         {
           headers:
             { 
-              nomeLoja: 'Loja A' 
+              nomeLoja: 'LojaA' 
             }
         }
-      ); // URL do seu backend
+    ); // URL do seu backend
       console.log("To em fetchProdutos 2");
       setProdutos(response); // Atualiza o estado com os dados retornados
       console.log("Response fetchProdutos:", response); // Exibe os dados no console
@@ -287,18 +292,9 @@ export default function TableProducts() {
       setProdutos([]); 
     }
   }
-
-
-
-  //GET
-  React.useEffect(() => {
-
-
-
   fetchProdutos();
   //console.log("produtosssssssss:", lp);
   //const rows = produtos;
-
 }, []);
 
   
