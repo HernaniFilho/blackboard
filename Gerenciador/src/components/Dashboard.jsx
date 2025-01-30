151465bb09aff08a8470c5458830c34afef6a90c
Emanuelle <emanuellegmnsa@gmail.com>
2025-01-30 12:37:32 -0300

    strategy?

--- Gerenciador/src/components/Dashboard.jsx
+++ Gerenciador/src/components/Dashboard.jsx
@@ -1,6 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useProdutosStore from "../state/ProdutoStore";
+import useTransferenciasStore from "../state/TransferenciaStore";
+import useComprasStore from "../state/CompraStore";
+import useFornecedoresStore from "../state/FornecedoresStore";
 
 function Dashboard() {
+  const { produtos, fetchProdutos, addProduto } = useProdutosStore();
+  const { fetchTransferencias, addTransferencia } = useTransferenciasStore();
+  const { fornecedores, fetchFornecedores } = useFornecedoresStore();
+  const { addCompra } = useComprasStore();
+
+  useEffect(() => {
+    fetchProdutos();
+
+    for (const produto of produtos) {
+      if (produto.quantidade <= produto.estoqueMin) {
+        //verificar a outra loja
+        // se não fazer compra
+      }
+    }
+  }, []);
   return (
     <>
       <div></div>
